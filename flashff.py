28ecdd375afba1c897e647db13d587271fdd0826
Bas Boot <b.boot@student.tudelft.nl>
2020-10-08 22:03:15 +0200

    Find all USB ports with FFs

--- flashff.py
+++ flashff.py
@@ -1,3 +1,20 @@
+import subprocess
+import sys
 
+assert ((sys.platform  == "darwin") or (sys.platform  == "linux")), "Platform unsupprted"
+
+# executable to find motes
+motelist = "./motelist-zolertia"
+
+# alternative version for mac-os
+if (sys.platform  == "darwin"):
+    motelist = "./motelist-zolertia-macos"
+
+output = (subprocess.Popen(["./motelist-zolertia-macos", "-c"], stdout=subprocess.PIPE).communicate()[0]).decode("utf-8")
+
+devices = output.splitlines()
+
+for device in devices:
+    print("Flashing device on USB: %s" % device.split(",")[1])
 
 
