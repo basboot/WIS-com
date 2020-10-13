da738f0b49a4cec392b5f1159bdbb3607e7d0228
Jacob Lont <lontjacob@hotmail.com>
2020-10-13 16:05:49 +0200

    Some adjustments to run on Raspbian (but still ttyUSB problems)

--- flashff.py
+++ flashff.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-assert ((sys.platform  == "darwin") or (sys.platform  == "linux")), "Platform unsupprted"
+assert ((sys.platform  == "darwin") or (sys.platform  == "linux") or (sys.platform == "linux2")), "Platform unsupprted"
 
 if (len(sys.argv) != 2):
     print ("Usage: python flashff.py subdirectory_with_crystal_bin")
@@ -18,15 +18,16 @@ motelist = "./motelist-zolertia"
 if (sys.platform  == "darwin"):
     motelist = "./motelist-zolertia-macos"
 
-output = (subprocess.Popen(["./motelist-zolertia-macos", "-c"], stdout=subprocess.PIPE).communicate()[0]).decode("utf-8")
+output = (subprocess.Popen([motelist, "-c"], stdout=subprocess.PIPE).communicate()[0]).decode("utf-8")
 
 devices = output.splitlines()
 
+print(devices)
 for device in devices:
     print("Flashing device on USB: '%s'" % device.split(",")[1])
     usb = device.split(",")[1]
 
-    flash_output = (subprocess.Popen(["python3", "cc2538-bsl.py", "-e", "-w",  "-v",  "-a 0x00200000",  "-b",  "460800",
+    flash_output = (subprocess.Popen(["python3", "cc2538-bsl.py", "-e", "-w",  "-v",  "-a 0x00200000",  "-b",  "115200",
                       "-p%s" % usb,  "%s/crystal-test.bin" % dir ], stdout=subprocess.PIPE).communicate()[0]).decode("utf-8")
 
     #print(flash_output)
